f34c980fe6bc9031d84ae00a52f2b6fa9e36190c
Alex Calderwood <alex@eduroam-169-233-205-114.ucsc.edu>
2022-02-01 13:58:23 -0800

    A few changes that make setting up the project easier.

--- src/external_model.py
+++ src/external_model.py
@@ -3,8 +3,8 @@ import os
 import time
 import twee_utils as utils
 
-openai.api_key = os.getenv("OPENAI_KEY")
-
+# Load your API key from an environment variable or secret management service
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def _call_naive_model(prompt):
     """
